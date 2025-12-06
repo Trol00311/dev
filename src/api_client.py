07a8153f2fa1abcb23c0f9d5f1ecb0a327c3ecf9
Trol00311 <80637774+Trol00311@users.noreply.github.com>
2025-12-06 19:50:54 +0100

    Update api_client.py

--- src/api_client.py
+++ src/api_client.py
@@ -20,3 +20,11 @@ class ApiClient:
         payload = {"title": name}
         response = requests.post(f"{self.base_url}/rooms", json=payload, headers=headers)
         return response
+
+    def get_room_details(self, room_id):
+        headers = {
+            "Authorization": f"Bearer {self.token}"
+        }
+        url = f"{self.base_url}/rooms/{room_id}"
+        response = requests.get(url, headers=headers)
+        return response
